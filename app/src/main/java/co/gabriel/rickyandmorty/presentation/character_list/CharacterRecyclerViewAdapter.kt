ce3902781309a0ae1af6eb2f8e0b508fdadf1768
Braian Stiven Peña Morales <braian7410@gmail.com>
2022-03-25 15:35:48 -0500

    * Implementación de interfaz gráfica para agregar/eliminar items al carrito * Implementación de lógica para agregar/eliminar items del carrito

--- app/src/main/java/co/gabriel/rickyandmorty/presentation/character_list/CharacterRecyclerViewAdapter.kt
+++ app/src/main/java/co/gabriel/rickyandmorty/presentation/character_list/CharacterRecyclerViewAdapter.kt
@@ -2,6 +2,7 @@ package co.gabriel.rickyandmorty.presentation.character_list
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -9,7 +10,7 @@ import co.gabriel.rickyandmorty.data.model.Character
 import co.gabriel.rickyandmorty.databinding.CharacterItemBinding
 import com.bumptech.glide.Glide
 
-class CharacterRecyclerViewAdapter(private var characters : List<Character> ) :
+class CharacterRecyclerViewAdapter(private var characters : List<Character>, private val itemClickListener: OnClickListener ) :
     RecyclerView.Adapter<CharacterRecyclerViewAdapter.ViewHolder>() {
 
 
@@ -23,20 +24,57 @@ class CharacterRecyclerViewAdapter(private var characters : List<Character> ) :
         return characters.size
     }
 
+    interface OnClickListener {
+        fun onButtonAddClick(
+            price: Int
+        )
+        fun onButtonRemoveClick(
+            price: Int,
+            subtotal: Int
+        )
+    }
+
+    private fun validateSubtotal(subtotal: Int): Boolean {
+        return subtotal != 0
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val character = characters[position]
         holder.name.text = character.name
-
+        val price = 10000
+        var totalItem: Int = holder.adder.text.toString().toInt()
+        var subtotal: Int = holder.subtotal.text.toString().toInt()
+        holder.price.text = price.toString()
+        holder.add.setOnClickListener{
+            subtotal += price
+            totalItem += 1
+            itemClickListener.onButtonAddClick(price)
+            holder.subtotal.text = subtotal.toString()
+            holder.adder.text = totalItem.toString()
+        }
+        holder.remove.setOnClickListener {
+            if (validateSubtotal(subtotal)){
+                itemClickListener.onButtonRemoveClick(price, subtotal)
+                subtotal -= price
+                totalItem -= 1
+                holder.subtotal.text = subtotal.toString()
+                holder.adder.text = totalItem.toString()
+            }
+        }
         Glide.with(holder.itemView)
             .load(character.image)
             .thumbnail(0.1f)
             .into(holder.image)
     }
 
-
     inner class ViewHolder(binding: CharacterItemBinding) : RecyclerView.ViewHolder(binding.root) {
         val image: ImageView = binding.characterImage
         val name : TextView = binding.characterName
+        val price : TextView = binding.characterPrice
+        val subtotal : TextView = binding.subtotalItem
+        val adder : TextView = binding.characterAdder
+        val add : ImageButton = binding.btnAdd
+        val remove : ImageButton = binding.btnRemove
     }
 
     fun updateCharacters(characters: List<Character>){
